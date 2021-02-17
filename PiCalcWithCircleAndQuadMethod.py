df638dd53114227f37db4d87b3e1ca547c0e363c
svante <svante.spannar@gmail.com>
2021-02-17 07:51:39 +0100

    longer, but not working

---
+++ PiCalcWithCircleAndQuadMethod.py
@@ -0,0 +1,25 @@
+def pi(distance):
+    x = 0.0
+    y = 0.0
+
+    total = 0
+    circle = 0
+
+    while y != 1:
+        if x == 1:
+            total += 1
+            y += distance
+
+        if x != 1:
+            print(y, x)
+            total += 1
+            x += distance
+            x = 0
+
+        if (x*y + y*y) < 1:
+            circle += 1
+
+    return 4*(circle/total)
+
+
+print(pi(.1))
